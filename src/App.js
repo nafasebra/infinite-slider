e38e73d64f9ab5e8c638df8093c9f1c3ab8c35d4
Nafiseh Ebrahimi <ebranafas@gmail.com>
2021-09-15 00:26:57 +0430

    add capture in slider

--- src/App.js
+++ src/App.js
@@ -29,12 +29,17 @@ class App extends Component {
               />
             </div>
 
+            <div className="slider-text"> 
+              <h2>Hello nature 1</h2>
+              <p>this is capture for project slider, this project created by react.js library. react.js is library for create UserInterface and SPA Websites</p>
+            </div>
+
             <div className="controls">
               <button className="prev-button">
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" /></svg>
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" /></svg>
               </button>
               <button className="next-button">
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" /></svg>  
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" /></svg>
               </button>
             </div>
 
