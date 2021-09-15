3681669698917ab5301445f868c4cfbc68044fdf
Nafiseh Ebrahimi <ebranafas@gmail.com>
2021-09-16 00:12:14 +0430

    setup slider (set state and add method controls)

--- src/App.js
+++ src/App.js
@@ -5,11 +5,55 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      topSlide: 0
+    }
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  prevSlide() { 
+    if(this.state.topSlide === 0) {
+      this.setState({
+        topSlide: -200
+      })
+    } else {
+      this.setState({
+        topSlide: this.state.topSlide + 100
+      })
+    }
+  }
+
+  nextSlide() {
+    if(this.state.topSlide === -200) {
+      this.setState({
+        topSlide: 0
+      })
+    } else {
+      this.setState({
+        topSlide: this.state.topSlide - 100
+      })
+    }
+  }
+
+
   render() {
     return (
       <div className="app-container">
-        <div className="slider-container">
-          <div className="slider">
+        <div 
+          className="slider-container"
+          style={{
+            position: 'absolute',
+            top: this.state.topSlide + '%'
+          }}
+        >
+          <div  className="slider"  >
             <div className="slider__item">
               <img 
                   src="https://www.wallpapers13.com/wp-content/uploads/2020/05/Yosemite-National-Park-Valley-in-California-USA-Mountains-Forests-Grass-Nature-Scenery-Wallpaper-Hd.jpg-915x515.jpg"
@@ -35,10 +79,16 @@ class App extends Component {
             </div>
 
             <div className="controls">
-              <button className="prev__button">
+              <button 
+                onClick={() => this.prevSlide()}
+                className="prev__button"
+              >
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" /></svg>
               </button>
-              <button className="next__button">
+              <button 
+                onClick={() => this.nextSlide()}
+                className="next__button"
+              >  
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" /></svg>
               </button>
             </div>
