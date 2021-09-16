15d688fa9d8bb58ef92fe835e16df4af745ae038
Nafiseh Ebrahimi <ebranafas@gmail.com>
2021-09-16 21:59:40 +0430

    set image of slider bassed on dataslider file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 // import Slider from './Components/Slider';
 
+// include data of slider
+import { DataSlider } from './Data/DataSlider';
+
 // import style
 import './App.css';
 
@@ -10,7 +13,7 @@ class App extends Component {
     super();
 
     this.state = {
-      topSlide: 0
+      topPosSlide: 0
     }
   }
 
@@ -19,25 +22,25 @@ class App extends Component {
   }
 
   prevSlide() { 
-    if(this.state.topSlide === 0) {
+    if(this.state.topPosSlide === 0) {
       this.setState({
-        topSlide: -200
+        topPosSlide: -200
       })
     } else {
       this.setState({
-        topSlide: this.state.topSlide + 100
+        topPosSlide: this.state.topPosSlide + 100
       })
     }
   }
 
   nextSlide() {
-    if(this.state.topSlide === -200) {
+    if(this.state.topPosSlide === -200) {
       this.setState({
-        topSlide: 0
+        topPosSlide: 0
       })
     } else {
       this.setState({
-        topSlide: this.state.topSlide - 100
+        topPosSlide: this.state.topPosSlide - 100
       })
     }
   }
@@ -50,20 +53,19 @@ class App extends Component {
           className="slider-container"
           style={{
             position: 'absolute',
-            top: this.state.topSlide + '%'
+            top: this.state.topPosSlide + '%'
           }}
         >
           <div className="slider">
-            {/* <div className="slider__item">
-              <img 
-                  src="https://img3.akspic.ru/originals/8/0/5/5/45508-nauka-nebo-ozero_pejto-pustynya-ekosistema-2048x1152.jpg" 
-                  alt="item-2" 
-              />
-            </div> */}
+            {
+              DataSlider.map( item => <div key={item.id} className="slider__item" >
+                                        <img src={item.srcImage} alt={item.altImage} />
+                                      </div>)
+            }
 
             <div className="caption"> 
               <h2 className="caption__title">Hello nature 1</h2>
-              <p className="caption__desc"></p>
+              <p className="caption__desc">this is capture for project slider, this project created by react.js library. react.js is library for create UserInterface and SPA Websites</p>
             </div>
 
             <div className="controls">
