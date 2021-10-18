1af50e9f853ca358d89464e5d268b8eeec7a9b78
unknown <ebranafas@gmail.com>
2021-10-18 20:26:03 +0330

    change method for translation items

--- src/App.js
+++ src/App.js
@@ -12,74 +12,34 @@ class App extends PureComponent {
     super();
 
     this.state = {
-      topPosSlide: 0,
       current: 0,
       captionOpacity: 1,
-      slides: []
+      slides: [...DataSlider],
+      direction: '',
+      yPositions: [0, 100, 200, 300]
     }
   }
 
 
-  componentDidMount() {
-    
-    this.setState({
-      slides: [
-        DataSlider[DataSlider.length - 1],
-        ...DataSlider
-      ]
-    })
-
-    console.log(this.state.slides);
-  }
-
   prevSlide() { 
 
-    let {current, slides, topPosSlide} = this.state;
-    const slideLength = DataSlider.length;
+    let {current, topPosSlide} = this.state;
+
+    let yPositions = this.state.yPositions.slice();
+
+    yPositions.unshift(yPositions.pop());
 
     this.setState({
-      topPosSlide: topPosSlide + 100,
-      current: current - 1,
-      slides: current === 0 && [...DataSlider.slice(0, slideLength - 2), ...slides]
+      //current: current - 1,
+      yPositions,
+      direction: 'up',
     })
-    
-    if(current <= -1) return;
 
-    
-    // handle animation
+
+    //handle animation
     requestAnimationFrame(() => {
       this.setState({
         captionOpacity: 0.5
-      })
-      setTimeout(() => {
-        this.setState({
-          captionOpacity: 1
-        })
-      }, 200);
-    })
-
-    console.log(this.state);
-  }
-
-  nextSlide() {
-
-    let { slides, current, topPosSlide } = this.state;
-    const slideLength = DataSlider.length;
-
-
-    this.setState({
-      topPosSlide: topPosSlide - 100,
-      current: current === slideLength - 1 ? 0 : current + 1,
-      slides: current === slideLength - 2 && 
-        [...slides, ...DataSlider]
-    })
-
-    if(current >= slideLength) return;
-
-    // handle animation
-    requestAnimationFrame(() => {
-      this.setState({
-        captionOpacity: 0
       })
       setTimeout(() => {
         this.setState({
@@ -91,24 +51,80 @@ class App extends PureComponent {
     console.log(this.state);
   }
 
+  nextSlide() {
+
+    let { current, topPosSlide } = this.state;
+
+    let yPositions = this.state.yPositions.slice();
+
+    yPositions.push(yPositions.shift());
+
+    this.setState({
+      //current: current + 1,
+      yPositions,
+      direction: 'down'
+    })
+
+
+    // handle animation
+    requestAnimationFrame(() => {
+      this.setState({
+        captionOpacity: 0.5
+      })
+      setTimeout(() => {
+        this.setState({
+          captionOpacity: 1
+        })
+      }, 500);
+    })
+    
+
+    console.log(this.state);
+  }
+
+  setTransition(index, isShow) {
+    let { yPositions } = this.state;
+    const pos = yPositions[index];
+
+    if(isShow) {
+      return {
+        transform: `translateY(${pos}%)`,
+        WebkitTransition: 'all 200ms linear',
+        MozTransition: 'all 200ms linear',
+        msTransition: 'all 200ms linear',
+        OTransition: 'all 200ms linear'
+      }
+    }
+  }
+
 
   render() {
-    let { slides, topPosSlide, current, captionOpacity } = this.state;
+    let { 
+      slides,
+      current, 
+      captionOpacity,
+      direction,
+      yPositions
+    } = this.state;
+
 
     return (
       <div className="app-container">
         <div 
           className="slider-container"
           style={{
-            position: 'absolute',
-            top: topPosSlide + '%'
+            position: 'absolute'
           }}
         >
           <div className="slider">
             {
-              slides.map( (item, index) => <div key={index} className="slider__item" >
-                                        <img src={item.srcImage} alt={item.altImage} />
-                                      </div>)
+              slides.map( (item, index) => {
+                let isShowTransition = (yPositions[index] > 0 && direction === 'down') || (yPositions[index] < 300 && direction === 'up');
+                let itemStyle = this.setTransition(index, isShowTransition);
+                <div key={item.id} className="slider__item" >
+                  <img src={item.srcImage} style={itemStyle} alt={item.altImage} />
+                </div>
+              })
             }
 
             <div className="caption"> 
@@ -118,12 +134,12 @@ class App extends PureComponent {
                   transition: 'all 0.5s'
                 }}
               >
-                <h2 className="caption__title">
-                  {DataSlider[current].captionTitle}
+                {/* <h2 className="caption__title">
+                  {slides[current].captionTitle}
                 </h2>
                 <p className="caption__desc">
-                  {DataSlider[current].captionDesc}
-                </p>
+                  {slides[current].captionDesc}
+                </p> */}
               </div>
             </div>
 
