294e088ec28ee11460671fb1ba282b39c865f295
unknown <ebranafas@gmail.com>
2021-10-23 11:23:05 +0330

    add infinite slider feature

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ class App extends PureComponent {
     this.state = {
       current: 0,
       captionOpacity: 1,
-      slides: [...DataSlider],
       direction: '',
       yPositions: [0, 100, 200, 300]
     }
@@ -23,16 +22,18 @@ class App extends PureComponent {
 
   prevSlide() { 
 
-    let {current, topPosSlide} = this.state;
+    let {current} = this.state;
 
     let yPositions = this.state.yPositions.slice();
 
-    yPositions.unshift(yPositions.pop());
+    yPositions.push(yPositions.shift());
+
+    if(current === 0) current = 4;
 
     this.setState({
-      //current: current - 1,
+      current: current - 1,
       yPositions,
-      direction: 'up',
+      direction: 'down',
     })
 
 
@@ -53,16 +54,18 @@ class App extends PureComponent {
 
   nextSlide() {
 
-    let { current, topPosSlide } = this.state;
+    let { current } = this.state;
 
     let yPositions = this.state.yPositions.slice();
 
-    yPositions.push(yPositions.shift());
+    yPositions.unshift(yPositions.pop());
+
+    if(current === DataSlider.length - 1) current = 1;
 
     this.setState({
-      //current: current + 1,
+      current: current + 1,
       yPositions,
-      direction: 'down'
+      direction: 'up'
     })
 
 
@@ -78,88 +81,77 @@ class App extends PureComponent {
       }, 500);
     })
     
-
-    console.log(this.state);
   }
 
-  setTransition(index, isShow) {
+  setTransition = (index) => {
     let { yPositions } = this.state;
     const pos = yPositions[index];
 
-    if(isShow) {
-      return {
-        transform: `translateY(${pos}%)`,
-        WebkitTransition: 'all 200ms linear',
-        MozTransition: 'all 200ms linear',
-        msTransition: 'all 200ms linear',
-        OTransition: 'all 200ms linear'
-      }
+    return {
+      transform: `translateY(${pos}%)`,
+      WebkitTransition: 'all 500ms ease-in-out',
+      MozTransition: 'all 500ms ease-in-out',
+      msTransition: 'all 500ms ease-in-out',
+      OTransition: 'all 500ms ease-in-out'
     }
   }
 
 
   render() {
     let { 
-      slides,
-      current, 
       captionOpacity,
-      direction,
-      yPositions
+      current
     } = this.state;
 
 
     return (
       <div className="app-container">
-        <div 
-          className="slider-container"
-          style={{
-            position: 'absolute'
-          }}
-        >
+        <div className="slider-container">
           <div className="slider">
-            {
-              slides.map( (item, index) => {
-                let isShowTransition = (yPositions[index] > 0 && direction === 'down') || (yPositions[index] < 300 && direction === 'up');
-                let itemStyle = this.setTransition(index, isShowTransition);
+          {
+            DataSlider.map((item, index) => {
+              let style = this.setTransition(index);
+              return (
                 <div key={item.id} className="slider__item" >
-                  <img src={item.srcImage} style={itemStyle} alt={item.altImage} />
+                  <img src={item.srcImage} style={style} alt={item.altImage} />
                 </div>
-              })
-            }
-
-            <div className="caption"> 
-              <div 
-                style={{
-                  opacity: captionOpacity,
-                  transition: 'all 0.5s'
-                }}
-              >
-                {/* <h2 className="caption__title">
-                  {slides[current].captionTitle}
-                </h2>
-                <p className="caption__desc">
-                  {slides[current].captionDesc}
-                </p> */}
-              </div>
-            </div>
-
-            <div className="controls">
-              <button 
-                onClick={() => this.prevSlide()}
-                className="prev__button"
-              >
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" /></svg>
-              </button>
-              <button 
-                onClick={() => this.nextSlide()}
-                className="next__button"
-              >  
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" /></svg>
-              </button>
-            </div>
-
-            <div className="slider-pagination"></div>
+              )
+            })
+          }
           </div>
+
+          <div className="caption"> 
+            <div 
+              style={{
+                opacity: captionOpacity,
+                transition: 'all 0.5s'
+              }}
+            >
+              <h2 className="caption__title">
+                {DataSlider[current].captionTitle}
+              </h2>
+              <p className="caption__desc">
+                {DataSlider[current].captionDesc}
+              </p>
+            </div>
+          </div>
+
+          <div className="controls">
+            <button 
+              onClick={() => this.prevSlide()}
+              className="prev__button"
+            >
+              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" /></svg>
+            </button>
+            <button 
+              onClick={() => this.nextSlide()}
+              className="next__button"
+            >  
+              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" /></svg>
+            </button>
+          </div>
+
+          <div className="slider-pagination"></div>
         </div>
       </div>
     );
